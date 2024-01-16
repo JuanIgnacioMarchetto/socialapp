242893ec9c0cec797f4da4ff765894db4af81e25
JuanIgnacioMarchetto <tu@email.com>
2024-01-16 14:08:19 -0300

    16/1/2024-14:08

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Post = ({ username, content, comments }) => {
   return (
-    <div>
+    <div className='conteinerpost'>
       <h3>{username}</h3>
+      <br></br>
       <p>{content}</p>
-      <ul>
+      <ul className="Post-comments">
         {comments.map((comment, index) => (
-          <li key={index}>{comment}</li>
+          <li key={index} className="Post-comment">
+            <p className="Post-comment-text">{comment}</p>
+          </li>
+          
         ))}
+              <br></br>
+
       </ul>
     </div>
   );
